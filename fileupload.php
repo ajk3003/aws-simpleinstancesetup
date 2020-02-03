fa7906e6ecd86bb9b0e15dd07439968b93e99af3
Antti Koskimäki <antti@koskimaki.net>
2020-02-03 12:20:13 +0200

    Modified php to work with IAM roles

--- fileupload.php
+++ fileupload.php
@@ -7,21 +7,25 @@ use Aws\Exception\AwsException;
 echo  "---Starting the PHP script--- -> ". "\n";
 if(isset($_POST["submit"])){
 
-  // Hard-coded credentials
-  // TODO: Change to utilize credentials in environmental variables
+  // The AWS PHP SDK will automatically check for instance IAM role
+  // credentials if anything is not specified upon the S3 client creation.
+  // As the EC2 instance has a role/policies assigned to it inline, in the
+  // terraform code, the SDK will then utilize credentials based on that role
+  //
+  // Couple other options for authentication would be hardcoding the
+  // credentials below or retrieving them from system variables, but utilizing
+  // the role/policy setup will not require one to upload the access keys
+  // to the EC2 instance or to typing them to this file directly.
   $s3Client = new S3Client([
       'version'     => 'latest',
-      'region'      => 'aws_region_of_the_bucket',
-      'credentials' => [
-          'key'    => 'your_aws_access_keyname',
-          'secret' => 'your_aws_secretkey',
-      ],
+      'region'      => 'your_aws_s3bucket_region'
   ]);
 
-  $b_name = 'your_s3_bucket_name';
+  $b_name = 'your_aws_bucket_name';
 
   $f_path = ($_FILES["fileToUpload"]["tmp_name"]);
   $f_name = basename($_FILES["fileToUpload"]["name"]);
+
   echo  "---Uploading file " . $f_name . " to the S3 bucket--- -> " . "\n";
   try {
       $result = $s3Client->putObject([
